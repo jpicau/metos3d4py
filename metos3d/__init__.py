5f21aa25eb830a95b18e10db6df4f0b225416b02
jpicau <jpi@informatik.uni-kiel.de>
2017-11-16 10:53:20 +0100

    cleaning up dir structure

---
+++ metos3d/__init__.py
@@ -0,0 +1,6 @@
+
+from ._version import version
+from .metos3d import init, run, final
+
+__all__ = ["version", "init", "run", "final"]
+
---
+++ metos3d/metos3d.py
@@ -0,0 +1,8 @@
+def init(argv):
+    print(argv)
+
+def run():
+    print("run")
+
+def final():
+    print("final")
